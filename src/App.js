917f60d96777a6ccb5a247b824f9847c48334f2d
cryptnology <jamie@cryptnology.dev>
2021-08-23 17:46:12 +1000

    Modified App.js to exclude NavBar

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import CreateCampaign from './components/CreateCampaign';
 import NotFound from './components/NotFound';
 import CampaignDetails from './components/CampaignDetails';
 import Container from 'react-bootstrap/Container';
-import NavBar from './components/NavBar';
+// import NavBar from './components/NavBar';
 
 const App = () => {
   const [account, setAccount] = useState(null);
@@ -35,7 +35,7 @@ const App = () => {
 
   return (
     <Web3Context.Provider value={web3User}>
-      <NavBar />
+      {/* <NavBar /> */}
       <Container>
         <Switch>
           <Route path='/campaigns/new' component={CreateCampaign} />
